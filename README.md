30a19b7072330403a60872c4ad20550ba1884cb8
Carpe-Wang <wangcarpe@gmail.com>
2025-03-31 15:48:17 -0400

    add more test case

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# task3
+# Task 3 Submission
+
+## GitHub Repository
+[https://github.com/Carpe-Wang/task3](https://github.com/Carpe-Wang/task3)
+
+## Docker Hub Image
+[https://hub.docker.com/r/your-docker-username/ci-helloworld](https://hub.docker.com/r/your-docker-username/ci-helloworld)
+
+## Notes
+- Coverage drops will fail the pipeline as expected.
+- All methods in `ArrayUtils` are tested with 100% coverage and 100% branch coverage.
--- src/test/java/org/example/task3/ArrayUtilsTest.java
+++ src/test/java/org/example/task3/ArrayUtilsTest.java
@@ -1,50 +1,61 @@
 package org.example.task3;
 
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 public class ArrayUtilsTest {
 
     @Test
-    public void testCountOf_basicCase() {
+    public void testCountOfWithMultipleOccurrences() {
         int[] arr = {1, 2, 3, 2, 4, 2};
         assertEquals(3, ArrayUtils.countOf(arr, 2));
     }
 
     @Test
-    public void testCountOf_noMatch() {
-        int[] arr = {1, 2, 3};
-        assertEquals(0, ArrayUtils.countOf(arr, 5));
+    public void testCountOfWithSingleOccurrence() {
+        int[] arr = {5, 6, 7};
+        assertEquals(1, ArrayUtils.countOf(arr, 6));
     }
 
     @Test
-    public void testCountOf_allMatch() {
-        int[] arr = {5, 5, 5};
-        assertEquals(3, ArrayUtils.countOf(arr, 5));
+    public void testCountOfWithNoOccurrence() {
+        int[] arr = {8, 9, 10};
+        assertEquals(0, ArrayUtils.countOf(arr, 1));
     }
 
     @Test
-    public void testFindMax_basicCase() {
-        int[] arr = {1, 2, 3, 5, 4};
+    public void testCountOfWithEmptyArray() {
+        int[] arr = {};
+        assertEquals(0, ArrayUtils.countOf(arr, 0));
+    }
+
+    @Test
+    public void testFindMaxWithMaxInMiddle() {
+        int[] arr = {1, 5, 3};
         assertEquals(5, ArrayUtils.findMax(arr));
     }
 
     @Test
-    public void testFindMax_firstIsMax() {
-        int[] arr = {9, 2, 3, 1};
-        assertEquals(9, ArrayUtils.findMax(arr));
+    public void testFindMaxWithMaxAtStart() {
+        int[] arr = {10, 2, 3};
+        assertEquals(10, ArrayUtils.findMax(arr));
     }
 
     @Test
-    public void testFindMax_lastIsMax() {
-        int[] arr = {1, 3, 5, 7, 9};
-        assertEquals(9, ArrayUtils.findMax(arr));
+    public void testFindMaxWithMaxAtEnd() {
+        int[] arr = {2, 3, 7};
+        assertEquals(7, ArrayUtils.findMax(arr));
     }
 
     @Test
-    public void testFindMax_allSame() {
+    public void testFindMaxWithAllSame() {
         int[] arr = {4, 4, 4};
         assertEquals(4, ArrayUtils.findMax(arr));
+    }
+
+    @Test
+    public void testFindMaxWithNegativeNumbers() {
+        int[] arr = {-10, -20, -3};
+        assertEquals(-3, ArrayUtils.findMax(arr));
     }
 }
