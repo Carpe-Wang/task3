3dc7f2c60c158b053bdea79c442115c7392ef092
Carpe-Wang <wangcarpe@gmail.com>
2025-03-31 16:08:45 -0400

    add more test case

--- README.md
+++ README.md
@@ -4,8 +4,8 @@
 [https://github.com/Carpe-Wang/task3](https://github.com/Carpe-Wang/task3)
 
 ## Docker Hub Image
-[https://hub.docker.com/r/your-docker-username/ci-helloworld](https://hub.docker.com/r/your-docker-username/ci-helloworld)
+[https://hub.docker.com/r/your-docker-username/ci-helloworld](https://hub.docker.com/r/wangcarpe/ci-helloworld)
 
 ## Notes
 - Coverage drops will fail the pipeline as expected.
-- All methods in `ArrayUtils` are tested with 100% coverage and 100% branch coverage.
+- The methods in `ArrayUtils` are tested with more than 90% coverage and 100% branch coverage.
--- src/test/java/org/example/task3/ArrayUtilsTest.java
+++ src/test/java/org/example/task3/ArrayUtilsTest.java
@@ -5,35 +5,35 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class ArrayUtilsTest {
 
-//    @Test
-//    public void testCountOfWithMultipleOccurrences() {
-//        int[] arr = {1, 2, 3, 2, 4, 2};
-//        assertEquals(3, ArrayUtils.countOf(arr, 2));
-//    }
-//
-//    @Test
-//    public void testCountOfWithSingleOccurrence() {
-//        int[] arr = {5, 6, 7};
-//        assertEquals(1, ArrayUtils.countOf(arr, 6));
-//    }
+    @Test
+    public void testCountOfWithMultipleOccurrences() {
+        int[] arr = {1, 2, 3, 2, 4, 2};
+        assertEquals(3, ArrayUtils.countOf(arr, 2));
+    }
 
-//    @Test
-//    public void testCountOfWithNoOccurrence() {
-//        int[] arr = {8, 9, 10};
-//        assertEquals(0, ArrayUtils.countOf(arr, 1));
-//    }
+    @Test
+    public void testCountOfWithSingleOccurrence() {
+        int[] arr = {5, 6, 7};
+        assertEquals(1, ArrayUtils.countOf(arr, 6));
+    }
 
-//    @Test
-//    public void testCountOfWithEmptyArray() {
-//        int[] arr = {};
-//        assertEquals(0, ArrayUtils.countOf(arr, 0));
-//    }
+    @Test
+    public void testCountOfWithNoOccurrence() {
+        int[] arr = {8, 9, 10};
+        assertEquals(0, ArrayUtils.countOf(arr, 1));
+    }
 
-//    @Test
-//    public void testFindMaxWithMaxInMiddle() {
-//        int[] arr = {1, 5, 3};
-//        assertEquals(5, ArrayUtils.findMax(arr));
-//    }
+    @Test
+    public void testCountOfWithEmptyArray() {
+        int[] arr = {};
+        assertEquals(0, ArrayUtils.countOf(arr, 0));
+    }
+
+    @Test
+    public void testFindMaxWithMaxInMiddle() {
+        int[] arr = {1, 5, 3};
+        assertEquals(5, ArrayUtils.findMax(arr));
+    }
 
     @Test
     public void testFindMaxWithMaxAtStart() {
